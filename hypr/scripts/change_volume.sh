6f46af7c6e57da777ed139dc99c6e6ce09decef9
O.Krasovskyi <mechanosec29@gmail.com>
2025-01-21 20:21:06 +0200

    dot save (2025-01-21 20:21:04)

---
+++ hypr/scripts/change_volume.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+param=$1
+
+if [ "$param" = "up" ]; then
+  wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+fi
+if [ "$param" = "down" ]; then
+  wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+fi
+volume=$(wpctl get-volume @DEFAULT_SINK@)
+notify-send "Change volume" "$volume"
