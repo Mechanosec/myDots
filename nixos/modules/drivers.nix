bab51868721c49e0b234cd8c03174a4d7d4e42ac
O.Krasovskyi <mechanosec29@gmail.com>
2025-01-29 13:29:11 +0200

    dot save (2025-01-29 13:29:09)

--- nixos/modules/drivers.nix
+++ nixos/modules/drivers.nix
@@ -15,5 +15,8 @@
     enable = true;
     wlr.enable = true;
     xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
   };
 }
--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -8,6 +8,7 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
+    MOZ_ENABLE_WAYLAND = "1";
     #ELECTRON_OZONE_PLATFORM_HINT = "auto";
   };
 
