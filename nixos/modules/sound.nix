c1c9bbdf934feb9b2607dc6771139813e5bc7cdb
O.Krasovskyi <mechanosec29@gmail.com>
2025-01-31 14:48:35 +0200

    dot save (2025-01-31 14:48:34)

--- nixos/modules/sound.nix
+++ nixos/modules/sound.nix
@@ -7,8 +7,11 @@
   services.pipewire = {
     enable = true;
 
+    wireplumber.enable = true;
+
     alsa.enable = true;
     alsa.support32Bit = true;
+
     pulse.enable = true;
 
     # If you want to use JACK applications, uncomment this
