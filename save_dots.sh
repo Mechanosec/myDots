604b30e619226a6ed23a7f15d7be9c3cb52e5651
O.Krasovskyi <mechanosec29@gmail.com>
2025-01-31 16:47:12 +0200

    dot save (2025-01-31 16:47:10)

--- save_dots.sh
+++ save_dots.sh
@@ -2,11 +2,10 @@
 
 current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
+rm -rf ~/.config/myNix/nixos
 cp -r /etc/nixos/ ~/.config/myNix/
 
 git pull
 git add .
 git commit -m "dot save ($current_datetime)"
 git push
-
-rm -rf ~/.config/myNix/nixos
